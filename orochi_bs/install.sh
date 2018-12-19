1f8825a208814f770e673193580a2a11d299075f
Raymond Cavalcante <cavalcante.raymond@gmail.com>
2018-12-19 13:05:29 -0500

    orochi_bs should use R3.4.4 and Bioc 3.6

--- orochi_bs/install.sh
+++ orochi_bs/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Change tag with each version of Bioconductor
-docker build --no-cache -t rcavalcante/orochi_bs:R3.5.1_Bioc3.8_v1 .
+docker build --no-cache -t rcavalcante/orochi_bs:R3.4.4_Bioc3.6_v1 .
 
 # Run with
 # docker run --interactive --tty --volume /path/to/folder/on/system:/path/within/docker orochi_epic bash
