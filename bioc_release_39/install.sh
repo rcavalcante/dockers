721b1a123befdbc1f14cb373dc5b6971bf5cbd90
Raymond Cavalcante <rcavalca@umich.edu>
2019-05-23 11:51:26 -0400

    Update install.sh and update.sh

--- bioc_release_39/install.sh
+++ bioc_release_39/install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Change tag with each version of Bioconductor
-docker build -t rcavalcante/r3.5.2_bioc3.8 .
+docker build -t rcavalcante/r3.6.0_bioc3.9 .
 
-docker push rcavalcante/r3.5.2_bioc3.8:latest
+docker push rcavalcante/r3.6.0_bioc3.9:latest
 
 # Run with
 # docker run --interactive --tty --volume /path/to/folder/on/system:/path/within/docker rcavalcante/r3.5.2_bioc3.8 bash
--- bioc_release_39/update.sh
+++ bioc_release_39/update.sh
@@ -5,6 +5,6 @@
 # You should commit the changes with the latest tag
 
 # This should only be run when ONE container is running and it is for bioc_devel_38
-docker container commit --pause $(docker ps -q) rcavalcante/r3.5.2_bioc3.8
+docker container commit --pause $(docker ps -q) rcavalcante/r3.6.0_bioc3.9
 
-docker push rcavalcante/r3.5.2_bioc3.8:latest
+docker push rcavalcante/r3.6.0_bioc3.9:latest
