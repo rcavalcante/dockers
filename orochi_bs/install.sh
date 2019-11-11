65c0e7b2cf87a37469182220317507985ec12b14
Raymond Cavalcante <rcavalca@umich.edu>
2019-11-11 14:02:37 -0500

    Remove bfxcore images; Add new Bioc images

--- orochi_bs/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Change tag with each version of Bioconductor
-docker build --no-cache -t rcavalcante/orochi_bs:R3.4.4_Bioc3.6_v1 .
-
-# Run with
-# docker run --interactive --tty --volume /path/to/folder/on/system:/path/within/docker orochi_epic bash
--- orochi_epic/update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Whenever packages are updated with:
-# BiocManager::install(ask = FALSE, update = TRUE)
-# You should commit the changes with the latest tag
-
-# This should only be run when ONE container is running
-docker container commit --pause $(docker ps -q) rcavalcante/orochi_epic:R3.5.1_R3.8v1
